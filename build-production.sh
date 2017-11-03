bc52c99fb6174af69cb43a85ea151d3ccea950f6
Dillon Hafer <dh@dillonhafer.com>
2017-11-03 18:28:25 -0500

    Optimize size of build artifacts
      Backend  (3.5 MB)
      Frontend (785 KB)

--- build-production.sh
+++ build-production.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+set -e
 
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-GOOS=linux GOARCH=386 buffalo build
+GOOS=linux GOARCH=386 buffalo bill --ldflags="-s -w"
 echo '----> compressing binary'
 cd bin
 tar czf budgetal.tar.gz budgetal
@@ -12,9 +13,18 @@ rm budgetal
 cd ..
 SIZE=$(du -h bin/budgetal.tar.gz | cut -f1)
 echo "----> linux 386 binary compressed in bin/budgetal.tar.gz ($SIZE)"
+
 echo "----> building frontend"
 cd frontend
 yarn build
+
+echo "----> removing sourcemaps"
+rm -rf build/users
+rm -f build/static/js/*.map
+rm -f build/static/css/*.map
+sed -i '' -e '/\/\/# sourceMappingURL.*/d' build/static/js/main*.js
+sed -i '' -e '/\/\*# sourceMappingURL.*/d' build/static/css/main*.css
+
 echo "----> compressing frontend"
 archive="frontend.tar.gz"
 tar czf $archive build
