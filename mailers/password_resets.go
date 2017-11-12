833e9a5533a2db26280da52a52d0cb86bc305718
Dillon Hafer <dh@dillonhafer.com>
2017-11-12 00:14:07 -0600

    Add email tests

--- mailers/password_resets.go
+++ mailers/password_resets.go
@@ -8,7 +8,7 @@ import (
 	"github.com/pkg/errors"
 )
 
-func SendPasswordResets(user *models.User) error {
+func BuildPasswordResetEmail(user *models.User) (mail.Message, error) {
 	to := user.Email
 	if user.FirstName != "" {
 		to = user.FirstName + " <" + user.Email + ">"
@@ -22,11 +22,21 @@ func SendPasswordResets(user *models.User) error {
 		"name":  user.FirstName,
 		"token": user.PasswordResetToken.String,
 	}
-	err := m.AddBodies(data, r.HTML("password_resets.html"), r.Plain("password_resets.txt"))
 
+	err := m.AddBodies(data, r.HTML("password_resets.html"), r.Plain("password_resets.txt"))
+	if err != nil {
+		return mail.NewMessage(), errors.WithStack(err)
+	}
+
+	return m, err
+}
+
+func SendPasswordResets(user *models.User) error {
+	m, err := BuildPasswordResetEmail(user)
 	if err != nil {
 		return errors.WithStack(err)
 	}
+
 	println(fmt.Sprintf("%#v", m))
 	return smtp.Send(m)
 }
