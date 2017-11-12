0bb010572809951e5a198885123534c993d6dca3
Dillon Hafer <dh@dillonhafer.com>
2017-11-12 13:26:04 -0600

    Test annual buget items update

--- actions/annual_budget_items_test.go
+++ actions/annual_budget_items_test.go
@@ -2,11 +2,12 @@ package actions
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/dillonhafer/budgetal-go/models"
 )
 
-func (as *ActionSuite) Test_AnnualBudgetItems_RequiresUser() {
+func (as *ActionSuite) Test_AnnualBudgetItems_Create_RequiresUser() {
 	r := as.JSON("/annual-budget-items").Post(AnnualBudgetItemParams{
 		Year:     2017,
 		Amount:   json.Number("0.00"),
@@ -23,7 +24,7 @@ func (as *ActionSuite) Test_AnnualBudgetItems_RequiresUser() {
 	as.Equal(401, r.Code)
 }
 
-func (as *ActionSuite) Test_AnnualBudgetItems_Create() {
+func (as *ActionSuite) Test_AnnualBudgetItems_Create_Works() {
 	user := SignedInUser(as)
 	b := models.AnnualBudget{Year: 2017, UserID: user.ID}
 	b.FindOrCreate(as.DB)
@@ -48,4 +49,49 @@ func (as *ActionSuite) Test_AnnualBudgetItems_Create() {
 	as.Equal("2017-12-12", rb.AnnualBudgetItem.DueDate)
 	as.Equal(false, rb.AnnualBudgetItem.Paid)
 	as.Equal(8, rb.AnnualBudgetItem.Interval)
+
+	all := models.AnnualBudgetItems{}
+	total, _ := as.DB.Count(&all)
+	as.Equal(1, total)
+}
+
+func (as *ActionSuite) Test_AnnualBudgetItems_Update_Works() {
+	user := SignedInUser(as)
+	b := models.AnnualBudget{Year: 2017, UserID: user.ID}
+	b.FindOrCreate(as.DB)
+	i := models.AnnualBudgetItem{
+		AnnualBudgetID: b.ID,
+		Amount:         json.Number("0.00"),
+		Name:           "Insurance",
+		DueDate:        "2017-12-12",
+		Paid:           false,
+		Interval:       8,
+	}
+	as.DB.Create(&i)
+
+	// Create Annual Budget
+	r := as.JSON(fmt.Sprintf("/annual-budget-items/%d", i.ID)).Put(AnnualBudgetItemParams{
+		Amount:   json.Number("10.00"),
+		Name:     "Life Insurance",
+		DueDate:  "2017-12-24",
+		Paid:     false,
+		Interval: 12,
+	})
+	var rb struct {
+		AnnualBudgetItem models.AnnualBudgetItem `json:"annualBudgetItem"`
+	}
+
+	json.NewDecoder(r.Body).Decode(&rb)
+	as.Equal(200, r.Code)
+	as.Equal(i.ID, rb.AnnualBudgetItem.ID)
+	as.Equal("Life Insurance", rb.AnnualBudgetItem.Name)
+	as.Equal(b.ID, rb.AnnualBudgetItem.AnnualBudgetID)
+	as.Equal("10.00", rb.AnnualBudgetItem.Amount.String())
+	as.Equal("2017-12-24", rb.AnnualBudgetItem.DueDate)
+	as.Equal(false, rb.AnnualBudgetItem.Paid)
+	as.Equal(12, rb.AnnualBudgetItem.Interval)
+
+	all := models.AnnualBudgetItems{}
+	total, _ := as.DB.Count(&all)
+	as.Equal(1, total)
 }
