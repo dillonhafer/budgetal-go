4329f949841260afeeea91632e736136fe056f6a
Dillon Hafer <dh@dillonhafer.com>
2017-11-12 20:27:23 -0600

    Test session delete

--- actions/actions_test.go
+++ actions/actions_test.go
@@ -36,18 +36,23 @@ func (as *ActionSuite) CreateUser(admin bool) models.User {
 	return user
 }
 
+func (as *ActionSuite) CreateSession(userId int) models.Session {
+	session := models.Session{
+		UserAgent:           "TEST",
+		AuthenticationToken: RandomHex(16),
+		UserID:              userId,
+		IpAddress:           "127.0.0.1",
+	}
+	session.Create(as.DB)
+	return session
+}
+
 func signInUser(admin bool, as *ActionSuite) models.User {
 	// Create User
 	user := as.CreateUser(admin)
 
 	// Create Session
-	session := &models.Session{
-		UserAgent:           "TEST",
-		AuthenticationToken: RandomHex(16),
-		UserID:              user.ID,
-		IpAddress:           "127.0.0.1",
-	}
-	session.Create(as.DB)
+	session := as.CreateSession(user.ID)
 
 	// Sign In User
 	cookie := fmt.Sprintf("_budgetal_session=%s; Expires=Tue, 09 Nov 2027 00:17:27 GMT; HttpOnly", session.AuthenticationKey)
--- actions/sessions_test.go
+++ actions/sessions_test.go
@@ -2,6 +2,7 @@ package actions
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/dillonhafer/budgetal-go/models"
 )
@@ -20,5 +21,18 @@ func (as *ActionSuite) Test_Sessions_Index() {
 
 func (as *ActionSuite) Test_Sessions_Index_RequiresUser() {
 	r := as.JSON("/sessions").Get()
+	as.Equal(401, r.Code)
+}
+
+func (as *ActionSuite) Test_Sessions_Delete() {
+	user := as.SignedInUser()
+	session := as.CreateSession(user.ID)
+
+	r := as.JSON(fmt.Sprintf("/sessions/%s", session.AuthenticationKey)).Delete()
+	as.Equal(200, r.Code)
+}
+
+func (as *ActionSuite) Test_Sessions_Delete_RequiresUser() {
+	r := as.JSON("/sessions/authKey").Delete()
 	as.Equal(401, r.Code)
 }
