634d5f193f8ee61313a786e18767ce98b6217152
Dillon Hafer <dh@dillonhafer.com>
2017-11-12 20:55:54 -0600

    Add sign out tests

--- actions/sign_out_test.go
+++ actions/sign_out_test.go
@@ -1,5 +1,25 @@
 package actions
 
-func (as *ActionSuite) Test_SignOut_SignOut() {
-	as.Equal(1, 1)
+import "github.com/dillonhafer/budgetal-go/models"
+
+func (as *ActionSuite) Test_SignOut() {
+	as.SignedInUser()
+
+	s := models.Sessions{}
+	as.DB.All(&s)
+	as.Equal(1, len(s))
+	as.False(s[0].ExpiredAt.Valid)
+
+	r := as.JSON("/sign-out").Delete()
+
+	as.Equal(200, r.Code)
+	s = models.Sessions{}
+	as.DB.All(&s)
+	as.Equal(1, len(s))
+	as.NotEmpty(s[0].ExpiredAt)
+}
+
+func (as *ActionSuite) Test_SignOut_RequiresUser() {
+	r := as.JSON("/sign-out").Delete()
+	as.Equal(401, r.Code)
 }
