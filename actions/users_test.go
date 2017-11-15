3d0371848f30c6937cc9ce5d8bdf12865c4f2026
Dillon Hafer <dh@dillonhafer.com>
2017-11-15 07:39:04 -0600

    Add register tests

--- actions/users_test.go
+++ actions/users_test.go
@@ -5,28 +5,97 @@ import (
 	"encoding/json"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/dillonhafer/budgetal-go/models"
+	"github.com/markbates/pop/nulls"
 )
 
 func (as *ActionSuite) Test_Users_UpdatePassword() {
-	as.Equal(1, 1)
+	user := as.CreateUser(false)
+	user.PasswordResetToken = nulls.String{String: "token", Valid: true}
+	user.PasswordResetSentAt = nulls.Time{Time: time.Now().Local().Add((-119 * time.Minute)), Valid: true}
+	as.DB.Update(&user)
+	as.True(user.PasswordResetToken.Valid)
+	as.True(user.PasswordResetSentAt.Valid)
+	as.False(user.VerifyPassword("new password"))
+
+	updatePasswordParams := map[string]string{
+		"password":             "new password",
+		"reset_password_token": "token",
+	}
+
+	r := as.JSON("/reset-password").Put(updatePasswordParams)
+	as.Equal(200, r.Code)
+	as.DB.Reload(&user)
+
+	as.False(user.PasswordResetToken.Valid)
+	as.False(user.PasswordResetSentAt.Valid)
+	as.True(user.VerifyPassword("new password"))
 }
 
 func (as *ActionSuite) Test_Users_UpdatePassword_TokenExpired() {
-	as.Equal(1, 1)
+	user := as.CreateUser(false)
+	user.PasswordResetToken = nulls.String{String: "token", Valid: true}
+	twoHoursAgo := time.Now().Local().Add((-120 * time.Minute))
+	user.PasswordResetSentAt = nulls.Time{Time: twoHoursAgo, Valid: true}
+	as.DB.Update(&user)
+	as.True(user.PasswordResetToken.Valid)
+	as.True(user.PasswordResetSentAt.Valid)
+	as.False(user.VerifyPassword("new password"))
+
+	updatePasswordParams := map[string]string{
+		"password":             "new password",
+		"reset_password_token": "token",
+	}
+
+	r := as.JSON("/reset-password").Put(updatePasswordParams)
+	as.Equal(401, r.Code)
+	as.DB.Reload(&user)
+
+	as.True(user.PasswordResetToken.Valid)
+	as.True(user.PasswordResetSentAt.Valid)
+	as.True(user.VerifyPassword("password"))
+	as.False(user.VerifyPassword("new password"))
 }
 
 func (as *ActionSuite) Test_Users_PasswordResetRequest() {
-	as.Equal(1, 1)
+	user := as.CreateUser(false)
+	as.False(user.PasswordResetToken.Valid)
+	as.False(user.PasswordResetSentAt.Valid)
+
+	r := as.JSON("/reset-password").Post(map[string]string{"email": "user@example.com"})
+	as.Equal(200, r.Code)
+	as.DB.Reload(&user)
+
+	as.True(user.PasswordResetToken.Valid)
+	as.True(user.PasswordResetSentAt.Valid)
 }
 
 func (as *ActionSuite) Test_Users_PasswordResetRequest_AlwaysWorks() {
-	as.Equal(1, 1)
+	r := as.JSON("/reset-password").Post(map[string]string{"email": "not.a.user@example.com"})
+	as.Equal(200, r.Code)
+	count, _ := as.DB.Count(&models.Users{})
+	as.Equal(0, count)
 }
 
 func (as *ActionSuite) Test_Users_Create() {
-	as.Equal(1, 1)
+	count, _ := as.DB.Count(&models.Users{})
+	as.Equal(0, count)
+
+	r := as.JSON("/register").Post(map[string]string{
+		"email":    "new.user@example.com",
+		"password": "password123",
+	})
+	as.Equal(200, r.Code)
+
+	count, _ = as.DB.Count(&models.Users{})
+	as.Equal(1, count)
+	user := models.User{}
+	as.DB.First(&user)
+
+	as.Equal("new.user@example.com", user.Email)
+	as.True(user.VerifyPassword("password123"))
 }
 
 func (as *ActionSuite) Test_Users_Update_RequiresUser() {
