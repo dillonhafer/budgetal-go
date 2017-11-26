aed8ef1de354a9f214948e7d3e5c965b207963d7
Dillon Hafer <dh@dillonhafer.com>
2017-11-26 06:24:17 -0600

    Add date tests to annual budgets

--- backend/actions/annual_budgets_test.go
+++ backend/actions/annual_budgets_test.go
@@ -1,6 +1,11 @@
 package actions
 
-import "github.com/dillonhafer/budgetal-go/backend/models"
+import (
+	"fmt"
+	"time"
+
+	"github.com/dillonhafer/budgetal-go/backend/models"
+)
 
 func (as *ActionSuite) Test_AnnualBudgets_Index_CreatesMissingBudget() {
 	SignedInUser(as)
@@ -22,12 +27,25 @@ func (as *ActionSuite) Test_AnnualBudgets_Index_CreatesMissingBudget() {
 
 func (as *ActionSuite) Test_AnnualBudgets_Index_BadYear() {
 	SignedInUser(as)
-
 	response := as.JSON("/annual-budgets/abcd").Get()
 	as.Equal(404, response.Code)
 }
 
+func (as *ActionSuite) Test_AnnualBudgets_Index_LowYear() {
+	SignedInUser(as)
+	response := as.JSON("/annual-budgets/2014").Get()
+	as.Equal(404, response.Code)
+}
+
+func (as *ActionSuite) Test_AnnualBudgets_Index_HighYear() {
+	SignedInUser(as)
+	url := fmt.Sprintf("/annual-budgets/%d", time.Now().Local().Year()+4)
+
+	response := as.JSON(url).Get()
+	as.Equal(404, response.Code)
+}
+
 func (as *ActionSuite) Test_AnnualBudgets_Index_RequiresUser() {
-	r := as.JSON("/annual-budgets/abcd").Get()
+	r := as.JSON("/annual-budgets/2017").Get()
 	as.Equal(401, r.Code)
 }
