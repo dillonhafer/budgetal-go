271357d4990cd0580ab7a569c17d96175aec52ff
Dillon Hafer <dh@dillonhafer.com>
2017-11-30 08:02:57 -0600

    Refactory monthly statistics to decode json better

--- backend/actions/monthly_statistics.go
+++ backend/actions/monthly_statistics.go
@@ -6,7 +6,6 @@ import (
 	"github.com/dillonhafer/budgetal-go/backend/models"
 	"github.com/fatih/color"
 	"github.com/gobuffalo/buffalo"
-	"github.com/markbates/pop"
 )
 
 func MonthlyStatisticsShow(c buffalo.Context, currentUser *models.User) error {
@@ -32,40 +31,28 @@ func MonthlyStatisticsShow(c buffalo.Context, currentUser *models.User) error {
 	err := map[string]string{"budgetCategories": ""}
 	type Category struct {
 		Name        string `json:"name" db:"name"`
-		AmountSpent string `json:"amountSpent" db:"amountSpent"`
+		AmountSpent string `json:"amountSpent" db:"amount_spent"`
 	}
 
 	query := `
     select
       bc.name,
-      coalesce(sum(bie.amount),0.00) as amountSpent
+      coalesce(sum(bie.amount),0.00) as amount_spent
     from budgets b
       join budget_categories bc on bc.budget_id = b.id
       left join budget_items bi on bi.budget_category_id=bc.id
       left join budget_item_expenses bie on bie.budget_item_id=bi.id
-    where b.month = :month
-      and b.year = :year
-      and b.user_id = :user_id
-    group by bc.id;
+    where b.month = ?
+      and b.year = ?
+      and b.user_id = ?
+    group by bc.id
   `
 	c.Logger().Debug(color.YellowString(query))
-
 	categories := []Category{}
-	tx := c.Value("tx").(*pop.Connection)
-	rows, dbErr := tx.TX.NamedQuery(query, params)
+	dbErr := models.DB.RawQuery(query, params.Month, params.Year, params.UserID).All(&categories)
 	if dbErr != nil {
 		c.Logger().Debug(dbErr)
 		return c.Render(500, r.JSON(err))
-	}
-
-	for rows.Next() {
-		var cat Category
-		rErr := rows.Scan(&cat.Name, &cat.AmountSpent)
-		if rErr != nil {
-			c.Logger().Debug(rErr)
-			return c.Render(500, r.JSON(err))
-		}
-		categories = append(categories, cat)
 	}
 
 	return c.Render(200, r.JSON(map[string][]Category{
