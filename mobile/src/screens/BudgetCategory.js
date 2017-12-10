878b6ee78819dba2cc8ee5b386e593e965b14ad0
Dillon Hafer <dh@dillonhafer.com>
2017-12-09 22:10:44 -0600

    Connect budget categories to redux

--- mobile/src/screens/BudgetCategory.js
+++ mobile/src/screens/BudgetCategory.js
@@ -5,42 +5,65 @@ import {
   Text,
   StatusBar,
   View,
+  FlatList,
 } from 'react-native';
+
+// Redux
+import { connect } from 'react-redux';
 
 // Components
 import { Ionicons } from '@expo/vector-icons';
 
 class BudgetCategoryScreen extends Component {
-  static navigationOptions = {
-    title: 'Charity',
-    tabBarIcon: ({ tintColor }) => (
-      <Ionicons name="md-calculator" size={32} color={tintColor} />
-    ),
+  renderItem = ({ item: budgetItem }) => {
+    const expenses = this.props.budgetItemExpenses.filter(e => {
+      return budgetItem.id === e.budgetItemId;
+    });
+
+    return (
+      <TouchableOpacity
+        style={styles.itemRow}
+        key={budgetItem.id}
+        onPress={() => {
+          this.props.navigation.navigate('BudgetItem', {
+            budgetItem,
+          });
+        }}
+      >
+        <View style={{ flexDirection: 'row' }}>
+          <Text>{budgetItem.name}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: '100%',
+          backgroundColor: '#CED0CE',
+        }}
+      />
+    );
   };
 
   render() {
+    const category = this.props.navigation.state.params.budgetCategory;
+    const items = this.props.budgetItems.filter(
+      i => i.budgetCategoryId === category.id,
+    );
     return (
       <View style={styles.container}>
         <StatusBar barStyle="dark-content" />
-        <Text
-          style={{
-            fontSize: 22,
-            fontWeight: '900',
-            color: '#444',
-            marginTop: 20,
-          }}
-        >
-          ---
-        </Text>
-        <TouchableOpacity
-          onPress={() => {
-            this.props.navigation.navigate('BudgetItem', {
-              budgetItem: { name: 'Gifts' },
-            });
-          }}
-        >
-          <Text>Gifts</Text>
-        </TouchableOpacity>
+        <FlatList
+          style={styles.list}
+          keyExtractor={i => i.id}
+          data={items}
+          ItemSeparatorComponent={this.renderSeparator}
+          renderItem={this.renderItem}
+        />
       </View>
     );
   }
@@ -49,10 +72,23 @@ class BudgetCategoryScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ececec',
+    backgroundColor: '#fff',
     alignItems: 'center',
     flexDirection: 'column',
   },
+  list: {
+    alignSelf: 'stretch',
+  },
+  itemRow: {
+    backgroundColor: '#fff',
+    padding: 15,
+    justifyContent: 'center',
+  },
 });
 
-export default BudgetCategoryScreen;
+export default connect(
+  state => ({
+    ...state.budget,
+  }),
+  dispatch => ({}),
+)(BudgetCategoryScreen);
