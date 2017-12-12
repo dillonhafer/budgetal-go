0308233085d910160dba0c35039145de43fe460a
Dillon Hafer <dh@dillonhafer.com>
2017-12-12 12:34:02 -0600

    Add change password screen

--- mobile/src/screens/ChangePassword.js
+++ mobile/src/screens/ChangePassword.js
@@ -1,28 +1,133 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, StatusBar, View } from 'react-native';
+import { StyleSheet, Text, TextInput, StatusBar, View } from 'react-native';
+
+// API
+import { ChangePasswordRequest } from 'api/users';
+
+// Helpers
+import { error, notice } from 'notify';
 
 // Components
-import { Ionicons } from '@expo/vector-icons';
+import { PrimaryButton, FieldContainer } from 'forms';
 
 class ChangePasswordScreen extends Component {
   static navigationOptions = {
     title: 'Change Password',
   };
 
+  inputs = [];
+
+  state = {
+    password: '',
+    passwordConfirmation: '',
+    currentPassword: '',
+    loading: false,
+  };
+
+  validateFields = () => {
+    const { currentPassword, password, passwordConfirmation } = this.state;
+    return (
+      password.length > 0 &&
+      currentPassword.length > 0 &&
+      passwordConfirmation.length > 0 &&
+      password === passwordConfirmation
+    );
+  };
+
+  focusNextField(key) {
+    this.inputs[key].focus();
+  }
+
+  changePassword = async () => {
+    const { password, currentPassword } = this.state;
+    const resp = await ChangePasswordRequest({ password, currentPassword });
+    if (resp && resp.ok) {
+      notice(resp.message);
+      this.props.navigation.goBack();
+    }
+  };
+
+  handleOnPress = () => {
+    this.setState({ loading: true });
+    try {
+      if (this.validateFields()) {
+        this.changePassword();
+      } else {
+        error('Form is not valid');
+      }
+    } catch (err) {
+      // console.log(err)
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
   render() {
+    const {
+      password,
+      passwordConfirmation,
+      currentPassword,
+      loading,
+    } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar barStyle="dark-content" />
-        <Text
-          style={{
-            fontSize: 22,
-            fontWeight: '900',
-            color: '#444',
-            marginTop: 20,
-          }}
-        >
-          ---
-        </Text>
+        <View style={{ height: 30 }} />
+        <FieldContainer>
+          <TextInput
+            style={{ height: 50 }}
+            enablesReturnKeyAutomatically={true}
+            secureTextEntry={true}
+            ref={input => {
+              this.inputs['password'] = input;
+            }}
+            placeholder="Password"
+            returnKeyType="next"
+            onSubmitEditing={_ => {
+              this.focusNextField('passwordConfirmation');
+            }}
+            onChangeText={password => this.setState({ password })}
+          />
+        </FieldContainer>
+        <FieldContainer>
+          <TextInput
+            style={{ height: 50 }}
+            enablesReturnKeyAutomatically={true}
+            secureTextEntry={true}
+            ref={input => {
+              this.inputs['passwordConfirmation'] = input;
+            }}
+            placeholder="Password Confirmation"
+            returnKeyType="next"
+            onSubmitEditing={_ => {
+              this.focusNextField('currentPassword');
+            }}
+            onChangeText={passwordConfirmation =>
+              this.setState({ passwordConfirmation })}
+          />
+        </FieldContainer>
+
+        <View style={{ height: 10 }} />
+        <FieldContainer>
+          <TextInput
+            style={{ height: 50 }}
+            enablesReturnKeyAutomatically={true}
+            secureTextEntry={true}
+            ref={input => {
+              this.inputs['currentPassword'] = input;
+            }}
+            placeholder="Current Password"
+            returnKeyType="done"
+            onSubmitEditing={this.handleOnPress}
+            onChangeText={currentPassword => this.setState({ currentPassword })}
+          />
+        </FieldContainer>
+
+        <PrimaryButton
+          title="Change Password"
+          onPress={this.handleOnPress}
+          loading={loading}
+        />
       </View>
     );
   }
