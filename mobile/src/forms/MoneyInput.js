a79770d06d4506043f1b445e2ccbd704b266da2c
Dillon Hafer <dh@dillonhafer.com>
2017-12-15 08:14:24 -0600

    Add new MoneyInput component

---
+++ mobile/src/forms/MoneyInput.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+import { View, Text, TouchableOpacity, Modal, StyleSheet } from 'react-native';
+import { currencyf } from 'utils/helpers';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import MoneyInputModal from 'forms/MoneyInputModal';
+
+class MoneyInput extends Component {
+  state = {
+    showMoneyPicker: false,
+  };
+
+  showMoneyPicker = () => {
+    this.setState({ showMoneyPicker: true });
+  };
+
+  hideMoneyPicker = () => {
+    this.setState({ showMoneyPicker: false });
+  };
+
+  handleOnSubmit = number => {
+    this.hideMoneyPicker();
+    this.props.onChange(number);
+  };
+
+  render() {
+    const { displayAmount, title } = this.props;
+    const { showMoneyPicker } = this.state;
+    return (
+      <TouchableOpacity
+        style={styles.amountButton}
+        onPress={this.showMoneyPicker}
+      >
+        <View>
+          <Text style={styles.displayAmount}>{currencyf(displayAmount)}</Text>
+          <MoneyInputModal
+            title={title || 'Enter Amount'}
+            visible={showMoneyPicker}
+            onOk={this.handleOnSubmit}
+            onCancel={this.hideMoneyPicker}
+          />
+        </View>
+        <MaterialCommunityIcons
+          name="chevron-right"
+          size={22}
+          style={{ paddingRight: 10 }}
+          color={'#ced0ce'}
+        />
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  amountButton: {
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+});
+
+export default MoneyInput;
---
+++ mobile/src/forms/MoneyInputModal.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import { TouchableOpacity, StyleSheet, Modal, View, Text } from 'react-native';
+
+// Components
+import { BlurView } from 'expo';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import colors from 'utils/colors';
+import { currencyf } from 'utils/helpers';
+import MoneyKeyboard from 'utils/MoneyKeyboard';
+import { PrimaryButton } from 'forms';
+
+class MoneyInputModal extends Component {
+  state = {
+    amount: '0',
+  };
+
+  onNumberChanged = number => {
+    this.setState({ amount: number });
+  };
+
+  onCancel = () => {
+    this.setState({ amount: '0' });
+    this.props.onCancel();
+  };
+
+  onOk = () => {
+    this.props.onOk(this.state.amount);
+  };
+
+  render() {
+    const { amount } = this.state;
+    const { title, visible, onOk, onCancel } = this.props;
+    const invalid = amount <= 0;
+
+    return (
+      <Modal visible={visible} animationType={'slide'} transparent={true}>
+        <BlurView tint="light" intensity={95} style={styles.modal}>
+          <TouchableOpacity style={styles.closeButton} onPress={this.onCancel}>
+            <Ionicons
+              name="ios-close-circle-outline"
+              size={24}
+              color={'#037aff'}
+            />
+          </TouchableOpacity>
+
+          <View style={styles.content}>
+            <View style={{ padding: 20, paddingTop: 40, alignItems: 'center' }}>
+              <FontAwesome name="money" size={32} color={colors.success} />
+              <Text
+                style={{ margin: 5, textAlign: 'center', fontWeight: 'bold' }}
+              >
+                {title}
+              </Text>
+            </View>
+
+            <PrimaryButton title="Done" onPress={this.onOk} loading={invalid} />
+          </View>
+
+          <MoneyKeyboard onChange={this.onNumberChanged} />
+        </BlurView>
+      </Modal>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  modal: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '100%',
+  },
+  closeButton: {
+    paddingLeft: 15,
+    paddingTop: 25,
+    paddingBottom: 15,
+  },
+});
+
+export default MoneyInputModal;
