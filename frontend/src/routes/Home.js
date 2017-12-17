47892dd25d072b88add3e3bd0f89c0dc5d48efaa
Dillon Hafer <dh@dillonhafer.com>
2017-12-16 20:55:54 -0600

    Update home screen with app store links

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { title, scrollTop } from 'window';
+import { Col, Row } from 'antd';
 
 class Home extends Component {
   componentDidMount() {
@@ -9,9 +10,66 @@ class Home extends Component {
 
   render() {
     return (
-      <div className="text-center">
-        <h1>Budgetal</h1>
-        <img alt="Budgetal" style={{ width: '64px' }} src={'/app_logo.png'} />
+      <div className="home-screen">
+        <Row type="flex" align="middle" justify="center">
+          <Col md={12} sm={24}>
+            <div className="text-right">
+              <img
+                src={require('images/app-logo.png')}
+                className="app-logo-image"
+                alt="budgetal"
+              />
+            </div>
+          </Col>
+          <Col md={12} sm={24}>
+            <Row type="flex" align="middle" justify="center">
+              <Col md={3} sm={24}>
+                <img
+                  alt="Budgetal"
+                  style={{ paddingLeft: '14px', width: '64px' }}
+                  src={'/app_logo.png'}
+                />
+              </Col>
+              <Col md={21} sm={24}>
+                <span className="app-title">Budgetal</span>
+              </Col>
+            </Row>
+            <Row type="flex" align="center" justify="center">
+              <Col span={24}>
+                <p className="app-subtitle">
+                  Because personal finances<br /> are personal.
+                </p>
+              </Col>
+            </Row>
+            <Row type="flex" align="center" justify="center">
+              <Col span={24}>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://itunes.apple.com/us/app/budgetal/id1326525398?mt=8"
+                >
+                  <img
+                    src={require('images/app-store.png')}
+                    style={{ height: '75px' }}
+                    alt="App Store"
+                  />
+                </a>
+
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://play.google.com/store/apps/details?id=com.budgetal.app"
+                >
+                  <img
+                    src={require('images/play-store.png')}
+                    style={{ height: '75px' }}
+                    alt="App Store"
+                  />
+                </a>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
       </div>
     );
   }
