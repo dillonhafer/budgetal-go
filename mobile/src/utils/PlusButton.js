b75a33e87d8d54f14e205b24353a45e43c6993a3
Dillon Hafer <dh@dillonhafer.com>
2018-02-06 15:22:28 -0600

    Allow + button to be disabled

--- mobile/src/utils/PlusButton.js
+++ mobile/src/utils/PlusButton.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
-const PlusButton = ({ onPress }) => {
+const PlusButton = ({ onPress, disabled }) => {
   return (
-    <TouchableOpacity onPress={onPress}>
+    <TouchableOpacity onPress={onPress} disabled={disabled}>
       <Ionicons
         name="ios-add-outline"
         size={32}
@@ -13,6 +13,7 @@ const PlusButton = ({ onPress }) => {
           fontWeight: '300',
           paddingRight: 20,
           paddingLeft: 20,
+          opacity: disabled ? 0.4 : 1,
         }}
       />
     </TouchableOpacity>
