e421b8a9fbd434103a9f9b7543ce5c4d5de5ba78
Dillon Hafer <dh@dillonhafer.com>
2018-02-17 12:36:27 -0600

    Update licenses

--- mobile/src/screens/account/Legal.js
+++ mobile/src/screens/account/Legal.js
@@ -46,6 +46,9 @@ class LegalScreen extends PureComponent {
 
             <Project>React, Copyright (c) 2013-present, Facebook, Inc.</Project>
             <Project>
+              React Native, Copyright (c) 2015-present, Facebook, Inc.
+            </Project>
+            <Project>
               React Redux, Copyright (c) 2015-present Dan Abramov
             </Project>
             <Project>Redux, Copyright (c) 2015-present Dan Abramov</Project>
@@ -63,59 +66,30 @@ class LegalScreen extends PureComponent {
               testshallpass
             </Project>
 
-            <Text style={styles.license}>
-              The MIT License (MIT) http://opensource.org/licenses/MIT/
-              Copyright (c) Permission is hereby granted, free of charge, to any
-              person obtaining a copy of this software and associated
-              documentation files (the "Software"), to deal in the Software
-              without restriction, including without limitation the rights to
-              use, copy, modify, merge, publish, distribute, sublicense, and/or
-              sell copies of the Software, and to permit persons to whom the
-              Software is furnished to do so, subject to the following
-              conditions: The above copyright notice and this permission notice
-              shall be included in all copies or substantial portions of the
-              Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
-              ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-              WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
-              AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-              HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-              WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-              OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-              DEALINGS IN THE SOFTWARE.
-            </Text>
+            <Project>UA parser js, Copyright © 2012-2016 Faisal Salman</Project>
 
-            <Text style={styles.licenseTitle}>BSD 2-clause license</Text>
-            <Text style={styles.subText}>
-              The following open source libraries are subject to the terms and
-              conditions of The BSD 2-clause license, included below.
-            </Text>
-            <Project>
-              React Native, Copyright (c) 2015-present, Facebook, Inc.
-            </Project>
-
-            <Text style={styles.license}>
-              The BSD 2-Clause License Copyright (c), All rights reserved.
-              Redistribution and use in source and binary forms, with or without
-              modification, are permitted provided that the following conditions
-              are met: 1. Redistributions of source code must retain the above
-              copyright notice, this list of conditions and the following
-              disclaimer. 2. Redistributions in binary form must reproduce the
-              above copyright notice, this list of conditions and the following
-              disclaimer in the documentation and/or other materials provided
-              with the distribution. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
-              HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
-              WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-              OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-              DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
-              BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-              EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-              TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-              DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-              ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-              TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
-              THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-              SUCH DAMAGE.
-            </Text>
+            <View style={styles.licenseContainer}>
+              <Text style={styles.license}>
+                The MIT License (MIT) http://opensource.org/licenses/MIT/
+                Copyright (c) Permission is hereby granted, free of charge, to
+                any person obtaining a copy of this software and associated
+                documentation files (the "Software"), to deal in the Software
+                without restriction, including without limitation the rights to
+                use, copy, modify, merge, publish, distribute, sublicense,
+                and/or sell copies of the Software, and to permit persons to
+                whom the Software is furnished to do so, subject to the
+                following conditions: The above copyright notice and this
+                permission notice shall be included in all copies or substantial
+                portions of the Software. THE SOFTWARE IS PROVIDED "AS IS",
+                WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+                NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+                PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+                AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+                OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+                OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+                SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+              </Text>
+            </View>
 
             <Text style={styles.licenseTitle}>BSD 3-clause license</Text>
             <Text style={styles.subText}>
@@ -136,53 +110,46 @@ class LegalScreen extends PureComponent {
               qs, Copyright (c) 2014 Nathan LaFreniere and other contributors.
             </Project>
 
-            <Text style={styles.license}>
-              Redistribution and use in source and binary forms, with or without
-              modification, are permitted provided that the following conditions
-              are met: 1. Redistributions of source code must retain the above
-              copyright notice, this list of conditions and the following
-              disclaimer. 2. Redistributions in binary form must reproduce the
-              above copyright notice, this list of conditions and the following
-              disclaimer in the documentation and/or other materials provided
-              with the distribution. 3. Neither the name of the copyright holder
-              nor the names of its contributors may be used to endorse or
-              promote products derived from this software without specific prior
-              written permission. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
-              HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
-              WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-              OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-              DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
-              BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-              EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-              TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-              DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-              ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-              TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
-              THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-              SUCH DAMAGE.
-            </Text>
+            <View style={styles.licenseContainer}>
+              <Text style={styles.license}>
+                Redistribution and use in source and binary forms, with or
+                without modification, are permitted provided that the following
+                conditions are met:
+              </Text>
 
-            <Text style={styles.licenseTitle}>
-              Dual licensed under GPLv2 & MIT
-            </Text>
-            <Text style={styles.subText}>
-              The following open source libraries are subject to the terms and
-              conditions of both the GPLv2 & MIT license, included below.
-            </Text>
+              <Text style={[styles.license, styles.number]}>
+                1. Redistributions of source code must retain the above
+                copyright notice, this list of conditions and the following
+                disclaimer.
+              </Text>
+              <Text style={[styles.license, styles.number]}>
+                2. Redistributions in binary form must reproduce the above
+                copyright notice, this list of conditions and the following
+                disclaimer in the documentation and/or other materials provided
+                with the distribution.
+              </Text>
+              <Text style={[styles.license, styles.number]}>
+                3. Neither the name of the copyright holder nor the names of its
+                contributors may be used to endorse or promote products derived
+                from this software without specific prior written permission.
+              </Text>
 
-            <Project>UA parser js, Copyright © 2012-2016 Faisal Salman</Project>
-
-            <Text style={styles.license}>
-              Permission is hereby granted, free of charge, to any person
-              obtaining a copy of this software and associated documentation
-              files (the "Software"), to deal in the Software without
-              restriction, including without limitation the rights to use, copy,
-              modify, merge, publish, distribute, sublicense, and/or sell copies
-              of the Software, and to permit persons to whom the Software is
-              furnished to do so, subject to the following conditions: The above
-              copyright notice and this permission notice shall be included in
-              all copies or substantial portions of the Software.
-            </Text>
+              <Text style={styles.license}>
+                THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+                CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+                INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+                MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+                DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+                CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+                SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+                LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+                USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+                AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+                LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+                IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+                THE POSSIBILITY OF SUCH DAMAGE.
+              </Text>
+            </View>
           </View>
         </ScrollView>
       </View>
@@ -206,12 +173,20 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 5,
   },
-  license: {
+  licenseContainer: {
     backgroundColor: '#ccc',
-    fontFamily: 'Menlo',
-    fontSize: 10,
+    borderRadius: 5,
+    overflow: 'hidden',
     padding: 10,
     marginBottom: 20,
+  },
+  license: {
+    fontFamily: 'Menlo',
+    fontSize: 10,
+  },
+  number: {
+    marginBottom: 10,
+    marginTop: 10,
   },
   licenseTitle: {
     fontSize: 20,
