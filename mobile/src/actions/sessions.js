c846db27037fad7107b6fabda56f9bba0c14f940
Dillon Hafer <dh@dillonhafer.com>
2018-02-17 13:41:08 -0600

    Remove unused reducer

--- mobile/src/actions/sessions.js
+++
@@ -1,7 +0,0 @@
-import { TOGGLE_DELETE } from 'redux-constants/action-types';
-
-export const toggleDelete = _ => {
-  return {
-    type: TOGGLE_DELETE,
-  };
-};
--- mobile/src/reducers/index.js
+++ mobile/src/reducers/index.js
@@ -2,14 +2,12 @@ import { combineReducers } from 'redux';
 
 import annualBudgetItems from 'reducers/AnnualBudgetItems';
 import budget from 'reducers/Budget';
-import sessions from 'reducers/Sessions';
 import users from 'reducers/Users';
 
 const appReducers = combineReducers({
   annualBudgetItems,
   budget,
   users,
-  sessions,
 });
 
 export default appReducers;
