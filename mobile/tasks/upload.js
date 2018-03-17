2995e8371dd0fe580c2954aa76f51326a6eb2137
Dillon Hafer <dh@dillonhafer.com>
2018-03-17 02:35:25 -0500

    Automate exp upload process

--- mobile/tasks/upload.js
+++ mobile/tasks/upload.js
@@ -1,6 +1,5 @@
 var fs = require('fs');
-var path = require('path');
-var downloads = `${process.env['HOME']}/Downloads`;
+const { exec } = require('child_process');
 const { abort, log } = require('./utils');
 
 // Get Credentials
@@ -11,29 +10,51 @@ if (user.toString() === '' || passwd.toString() === '') {
   abort('No username/password');
 }
 
-// Upload file
-fs.readdir(downloads, (err, files) => {
-  if (err !== null) {
-    abort(err);
-  }
+// Prepare for file download
+const fileName = 'tmp/tmp-ipa-name.ipa';
+const createTmpDir = next => {
+  log(`Preparing for file download`);
+  exec('mkdir -p tmp', err => {
+    if (err) {
+      abort(err);
+      return;
+    }
 
-  const file = files.find(file => {
-    return path.extname(file) === '.ipa';
+    next();
   });
-  if (file === undefined) {
-    abort('Could not find IPA');
-  }
-  const ipa = { file, fullPath: [downloads, file].join('/') };
+};
 
-  const validate = `altool --validate-app -f ${
-    ipa.fullPath
-  } -u ${user} -p '${passwd}'`;
-  const upload = `altool --upload-app -f ${
-    ipa.fullPath
-  } -u ${user} -p '${passwd}'`;
+// Get expo download Url
+const getDownloadUrl = next => {
+  log(`Getting download url`);
+  exec('exp url:ipa', (err, stdout) => {
+    if (err) {
+      abort(err);
+      return;
+    }
+    const downloadUrl = stdout.trim();
+    next(downloadUrl);
+  });
+};
 
-  log(`Uploading file: ${ipa.file}`);
-  const { exec } = require('child_process');
+// Download from expo
+const download = (downloadUrl, next) => {
+  log(`Downloading file from`);
+  log(`${downloadUrl}`);
+  log(`to ${fileName}`);
+  exec(`wget -q -O ${fileName} ${downloadUrl}`, err => {
+    if (err) {
+      abort(err);
+      return;
+    }
+    next();
+  });
+};
+
+// Validate with iTunes
+const validate = next => {
+  log(`Validating with iTunes`);
+  const validate = `altool --validate-app -f ${fileName} -u ${user} -p '${passwd}'`;
   exec(validate, (err, stdout, stderr) => {
     if (err) {
       abort(err);
@@ -41,8 +62,14 @@ fs.readdir(downloads, (err, files) => {
     }
     log(stdout);
     log(stderr);
+    next();
   });
+};
 
+// Upload to iTunes
+const uploadTask = () => {
+  const upload = `altool --upload-app -f ${fileName} -u ${user} -p '${passwd}'`;
+  log(`Uploading to iTunes`);
   exec(upload, (err, stdout, stderr) => {
     if (err) {
       abort(err);
@@ -50,5 +77,17 @@ fs.readdir(downloads, (err, files) => {
     }
     log(stdout);
     log(stderr);
+    log('Done.');
+  });
+};
+
+// Main
+createTmpDir(() => {
+  getDownloadUrl(url => {
+    download(url, () => {
+      validate(() => {
+        uploadTask();
+      });
+    });
   });
 });
