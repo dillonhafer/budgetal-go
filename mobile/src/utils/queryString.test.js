f8d68ef113462a7d61b08f64b44375ce2669d140
Dillon Hafer <dh@dillonhafer.com>
2018-05-01 22:50:21 -0500

    Fix no-named-as-default

--- mobile/src/utils/queryString.test.js
+++ mobile/src/utils/queryString.test.js
@@ -1,20 +1,20 @@
-import queryString from './queryString';
+import qs from './queryString';
 
 describe('queryString', () => {
   it('handles empty query string', () => {
-    expect(queryString('http://example.com/foobar')).toEqual({});
+    expect(qs('http://example.com/foobar')).toEqual({});
   });
 
   it('finds the first item', () => {
     expect(
-      queryString('http://example.com/foobar?reset_password_token=token')
+      qs('http://example.com/foobar?reset_password_token=token')
         .reset_password_token,
     ).toEqual('token');
   });
 
   it('finds the second item', () => {
     expect(
-      queryString(
+      qs(
         'http://example.com/foobar?email=hi@mail.com&reset_password_token=token',
       ).reset_password_token,
     ).toEqual('token');
@@ -22,7 +22,7 @@ describe('queryString', () => {
 
   it('handles URL encoded values', () => {
     expect(
-      queryString(
+      qs(
         'http://example.com/foobar?email=hi%40mail.com&reset_password_token=token',
       ).email,
     ).toEqual('hi@mail.com');
