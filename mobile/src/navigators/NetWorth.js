6940461b5245ffa2febd207a1813e657058a0fca
Dillon Hafer <dh@dillonhafer.com>
2018-08-29 19:29:13 -0400

    Fix sidebar screens for net worths

--- mobile/src/navigators/NetWorth.js
+++ mobile/src/navigators/NetWorth.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { HeaderText, BudgetalText } from 'components/Text';
 
@@ -14,89 +15,84 @@ import EditAssetLiabilityScreen from 'screens/net-worth/EditAssetLiability';
 
 import {
   NavigationHeight,
-  SidebarNavigationHeight,
   drawerIcon,
   BurgerNavigationOptions,
   BlurViewNavigationOptions,
 } from 'utils/navigation-helpers';
-const sidebarHeaderStyle = {
-  height: SidebarNavigationHeight,
-  width: 300,
-};
 const headerStyle = {
   height: NavigationHeight,
 };
 
-const NetWorthNavigatorStack = StackNavigator(
-  {
-    NetWorthScreen: {
-      screen: NetWorthScreen,
-      navigationOptions: () => ({
-        headerTitle: <HeaderText>NET WORTH</HeaderText>,
-        ...BurgerNavigationOptions,
-        ...BlurViewNavigationOptions,
-      }),
-    },
-    MonthListScreen: {
-      screen: MonthListScreen,
-      navigationOptions: ({ navigation }) => ({
-        headerTitle: (
-          <HeaderText>
-            {navigation.getParam('month').label.toUpperCase()}{' '}
-            {navigation.getParam('year')}
-          </HeaderText>
-        ),
-        headerStyle,
-        ...BlurViewNavigationOptions,
-      }),
-    },
-    EditMonthItemScreen: {
-      screen: EditMonthItemScreen,
-      navigationOptions: ({ navigation }) => ({
-        headerTitle: (
-          <HeaderText>
-            EDIT {navigation.getParam('item').name.toUpperCase()}
-          </HeaderText>
-        ),
-        headerStyle,
-        ...BlurViewNavigationOptions,
-      }),
-    },
-    NewMonthItemScreen: {
-      screen: NewMonthItemScreen,
-      navigationOptions: ({ navigation }) => ({
-        headerTitle: (
-          <HeaderText>
-            NEW {navigation.getParam('title').toUpperCase()}
-          </HeaderText>
-        ),
-        headerStyle,
-        ...BlurViewNavigationOptions,
-      }),
-    },
-    NewAssetLiabilityScreen: {
-      screen: NewAssetLiabilityScreen,
-      navigationOptions: ({ navigation }) => ({
-        headerTitle: (
-          <HeaderText>NEW {navigation.getParam('title')}</HeaderText>
-        ),
-        headerStyle,
-        ...BlurViewNavigationOptions,
-      }),
-    },
-    EditAssetLiabilityScreen: {
-      screen: EditAssetLiabilityScreen,
-      navigationOptions: ({ navigation }) => ({
-        headerTitle: (
-          <HeaderText>
-            EDIT {navigation.getParam('item').name.toUpperCase()}
-          </HeaderText>
-        ),
-        headerStyle,
-        ...BlurViewNavigationOptions,
-      }),
-    },
+const screens = {
+  NetWorthScreen: {
+    screen: NetWorthScreen,
+    navigationOptions: () => ({
+      headerTitle: <HeaderText>NET WORTH</HeaderText>,
+      ...BurgerNavigationOptions,
+      ...BlurViewNavigationOptions,
+    }),
   },
+  MonthListScreen: {
+    screen: MonthListScreen,
+    navigationOptions: ({ navigation }) => ({
+      headerTitle: (
+        <HeaderText>
+          {navigation.getParam('month').label.toUpperCase()}{' '}
+          {navigation.getParam('year')}
+        </HeaderText>
+      ),
+      headerStyle,
+      ...BlurViewNavigationOptions,
+    }),
+  },
+  EditMonthItemScreen: {
+    screen: EditMonthItemScreen,
+    navigationOptions: ({ navigation }) => ({
+      headerTitle: (
+        <HeaderText>
+          EDIT {navigation.getParam('item').name.toUpperCase()}
+        </HeaderText>
+      ),
+      headerStyle,
+      ...BlurViewNavigationOptions,
+    }),
+  },
+  NewMonthItemScreen: {
+    screen: NewMonthItemScreen,
+    navigationOptions: ({ navigation }) => ({
+      headerTitle: (
+        <HeaderText>
+          NEW {navigation.getParam('title').toUpperCase()}
+        </HeaderText>
+      ),
+      headerStyle,
+      ...BlurViewNavigationOptions,
+    }),
+  },
+  NewAssetLiabilityScreen: {
+    screen: NewAssetLiabilityScreen,
+    navigationOptions: ({ navigation }) => ({
+      headerTitle: <HeaderText>NEW {navigation.getParam('title')}</HeaderText>,
+      headerStyle,
+      ...BlurViewNavigationOptions,
+    }),
+  },
+  EditAssetLiabilityScreen: {
+    screen: EditAssetLiabilityScreen,
+    navigationOptions: ({ navigation }) => ({
+      headerTitle: (
+        <HeaderText>
+          EDIT {navigation.getParam('item').name.toUpperCase()}
+        </HeaderText>
+      ),
+      headerStyle,
+      ...BlurViewNavigationOptions,
+    }),
+  },
+};
+
+const NetWorthNavigatorStack = StackNavigator(
+  { ...screens },
   {
     cardStyle: {
       backgroundColor: '#ececec',
@@ -107,16 +103,18 @@ const NetWorthNavigatorStack = StackNavigator(
 
 const NetWorthSidebarNavigatorStack = StackNavigator(
   {
-    MonthListScreen: {
-      screen: MonthListScreen,
-      navigationOptions: screenProps => ({
-        headerStyle: sidebarHeaderStyle,
-        title: `${screenProps.month.name} 2019`,
-        ...BurgerNavigationOptions,
-      }),
+    Main: {
+      screen: View,
+      navigationOptions: { header: null },
     },
+    ...screens,
   },
-  {},
+  {
+    cardStyle: {
+      shadowOpacity: 0,
+    },
+    navigationOptions: BlurViewNavigationOptions,
+  },
 );
 
 class NetWorthNavigator extends TabletNavigator {
