48eae556d7a0d9c783f93325b474f3908a502042
Dillon Hafer <dh@dillonhafer.com>
2018-09-02 23:46:52 -0400

    Add Networth PN

--- backend/grifts/push_notifications.go
+++ backend/grifts/push_notifications.go
@@ -32,6 +32,39 @@ var _ = Set("new-month-reminders", func(c *Context) error {
 	return nil
 })
 
+var _ = Desc("net-worth-reminders", "Remind users to update their net worth")
+var _ = Set("net-worth-reminders", func(c *Context) error {
+	t := time.Now()
+	year := t.Year()
+	month := int(t.Month())
+
+	if month == 1 {
+		month = 12
+		year = year - 1
+	} else {
+		month = month - 1
+	}
+
+	nw := &models.NetWorths{}
+	models.DB.Select("distinct(user_id) as user_id").Where("year = ? and month = ? ", year, month).Join("net_worth_items", "net_worth_items.net_worth_id=net_worths.id").All(nw)
+	userIDs := make([]interface{}, len(*nw))
+	for i, n := range *nw {
+		userIDs[i] = n.UserID
+	}
+
+	users := &models.Users{}
+	models.DB.Where("id in (?)", userIDs...).All(users)
+
+	title := "📈💵 Record Your Progress"
+	body := "Don't forget to update your net worth!"
+	for _, user := range *users {
+		user.SendPushNotification(title, body)
+		time.Sleep(1 * time.Second)
+	}
+
+	return nil
+})
+
 func daysLeftInMonth() int {
 	now := time.Now()
 	currentYear, currentMonth, _ := now.Date()
