b2067577e5154d78a9acc80164f1356bf6c28f46
Dillon Hafer <dh@dillonhafer.com>
2019-01-09 11:37:17 -0500

    Delete asset/liability items from a given month

--- frontend/src/routes/NetWorth/AssetTable.js
+++ frontend/src/routes/NetWorth/AssetTable.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+
+// Redux
+import { connect } from 'react-redux';
+import { deleteNetWorthItem } from 'actions/net-worth-items';
+
 import {
   IconButton,
   Menu,
@@ -9,6 +14,8 @@ import {
 } from 'evergreen-ui';
 import PropTypes from 'prop-types';
 import { currencyf } from '@shared/helpers';
+import { notice, error } from 'window';
+import DeleteConfirmation from './DeleteConfirmation';
 
 class AssetTable extends Component {
   static propTypes = {
@@ -22,61 +29,121 @@ class AssetTable extends Component {
     emptyText: PropTypes.string,
   };
 
+  state = {
+    isSubmitting: false,
+    showDeleteConfirmation: false,
+    item: null,
+  };
+
+  handleOnDelete = item => {
+    this.setState({ isSubmitting: true });
+
+    this.props
+      .deleteNetWorthItem(item)
+      .then(() => {
+        notice(`${item.name} Deleted`);
+      })
+      .finally(() => {
+        this.setState({
+          isSubmitting: false,
+          showDeleteConfirmation: false,
+          item: null,
+        });
+      })
+      .catch(() => {
+        error('Something went wrong');
+      });
+  };
+
+  handleOnEdit = item => {
+    toaster.danger(`${item.name}: Not Implemented Yet`);
+  };
+
   render() {
     const { items, emptyText } = this.props;
 
     return (
-      <Table>
-        <Table.Head accountForScrollbar>
-          <Table.TextHeaderCell>Name</Table.TextHeaderCell>
-          <Table.TextHeaderCell>Amount</Table.TextHeaderCell>
-          <Table.TextHeaderCell />
-        </Table.Head>
-        <Table.Body>
-          {items.map(item => (
-            <Table.Row key={`${item.id}`}>
-              <Table.TextCell>{item.name}</Table.TextCell>
-              <Table.TextCell isNumber>{currencyf(item.amount)}</Table.TextCell>
-              <Table.TextCell align="right">
-                <Popover
-                  position={Position.BOTTOM_LEFT}
-                  content={
-                    <Menu>
-                      <Menu.Group>
-                        <Menu.Item
-                          icon="edit"
-                          onSelect={() => toaster.danger('Not Implemented Yet')}
-                        >
-                          Change Amount
-                        </Menu.Item>
-                      </Menu.Group>
-                      <Menu.Divider />
-                      <Menu.Group>
-                        <Menu.Item
-                          icon="trash"
-                          intent="danger"
-                          onSelect={() => toaster.danger('Not Implemented Yet')}
-                        >
-                          Delete
-                        </Menu.Item>
-                      </Menu.Group>
-                    </Menu>
-                  }
-                >
-                  <IconButton appearance="minimal" icon="more" />
-                </Popover>
-              </Table.TextCell>
-            </Table.Row>
-          ))}
-          {items.length === 0 && (
-            <Table.Row>
-              <Table.TextCell>{emptyText}</Table.TextCell>
-            </Table.Row>
-          )}
-        </Table.Body>
-      </Table>
+      <React.Fragment>
+        <Table>
+          <Table.Head accountForScrollbar>
+            <Table.TextHeaderCell>Name</Table.TextHeaderCell>
+            <Table.TextHeaderCell>Amount</Table.TextHeaderCell>
+            <Table.TextHeaderCell />
+          </Table.Head>
+          <Table.Body>
+            {items.map(item => (
+              <Table.Row key={`${item.id}`}>
+                <Table.TextCell>{item.name}</Table.TextCell>
+                <Table.TextCell isNumber>
+                  {currencyf(item.amount)}
+                </Table.TextCell>
+                <Table.TextCell align="right">
+                  <Popover
+                    position={Position.BOTTOM_LEFT}
+                    content={
+                      <Menu>
+                        <Menu.Group>
+                          <Menu.Item
+                            icon="edit"
+                            onSelect={() => this.handleOnEdit(item)}
+                          >
+                            Change Amount
+                          </Menu.Item>
+                        </Menu.Group>
+                        <Menu.Divider />
+                        <Menu.Group>
+                          <Menu.Item
+                            icon="trash"
+                            intent="danger"
+                            onSelect={() => {
+                              this.setState({
+                                showDeleteConfirmation: true,
+                                item,
+                              });
+                            }}
+                          >
+                            Delete
+                          </Menu.Item>
+                        </Menu.Group>
+                      </Menu>
+                    }
+                  >
+                    <IconButton appearance="minimal" icon="more" />
+                  </Popover>
+                </Table.TextCell>
+              </Table.Row>
+            ))}
+            {items.length === 0 && (
+              <Table.Row>
+                <Table.TextCell>{emptyText}</Table.TextCell>
+              </Table.Row>
+            )}
+          </Table.Body>
+        </Table>
+        <DeleteConfirmation
+          title={`Are you sure?`}
+          message={`Do you want to delete ${this.state.item &&
+            this.state.item.name}`}
+          isShown={this.state.showDeleteConfirmation}
+          isConfirmLoading={this.state.isSubmitting}
+          onConfirm={() => {
+            this.handleOnDelete(this.state.item);
+          }}
+          onCloseComplete={() => {
+            this.setState({
+              showDeleteConfirmation: false,
+              item: null,
+            });
+          }}
+        />
+      </React.Fragment>
     );
   }
 }
 
-export default AssetTable;
+export default connect(
+  null,
+  dispatch => ({
+    deleteNetWorthItem: item => dispatch(deleteNetWorthItem({ item })),
+  }),
+)(AssetTable);
