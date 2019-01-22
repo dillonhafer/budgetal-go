dee17e594b13e5e01052dbb5aa30b35f81e7acfa
Dillon Hafer <dh@dillonhafer.com>
2019-01-22 09:09:39 -0500

    Remove antd from Footer

--- frontend/src/layouts/Footer.js
+++ frontend/src/layouts/Footer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Layout, Modal } from 'antd';
+import { Paragraph, Pane, Text, Dialog } from 'evergreen-ui';
 import { Link } from 'react-router-dom';
+import { colors } from '@shared/theme';
 const HELP_FRAME = process.env.REACT_APP_HELP_FRAME || '';
 
 export default class Footer extends Component {
@@ -18,11 +19,26 @@ export default class Footer extends Component {
 
   render() {
     return (
-      <Layout.Footer>
-        Budgetal © 2013-
-        {new Date().getFullYear()} All rights reserved
-        <p>
-          <Link to="/privacy">Privacy</Link> |{' '}
+      <Pane textAlign="center" paddingBottom={32}>
+        <Text>
+          Budgetal © 2013-
+          {new Date().getFullYear()} All rights reserved
+        </Text>
+        <Paragraph>
+          <Link
+            style={{
+              background: 'none',
+              border: 'none',
+              outline: 'none',
+              cursor: 'pointer',
+              textDecoration: 'underline',
+              color: '#444',
+            }}
+            to="/privacy"
+          >
+            Privacy
+          </Link>{' '}
+          |{' '}
           <button
             style={{
               background: 'none',
@@ -36,19 +52,30 @@ export default class Footer extends Component {
           >
             Help
           </button>
-        </p>
-        <Modal
-          title=""
-          wrapClassName="help-modal"
-          visible={this.state.showHelpModal}
-          footer={null}
+        </Paragraph>
+        <Dialog
+          title="Budgetal Help"
+          isShown={this.state.showHelpModal}
+          preventBodyScrolling
+          hasFooter={false}
           onOk={this.handleHelpClose}
           onCancel={this.handleHelpClose}
+          padding={0}
         >
-          <iframe title="help" src={HELP_FRAME} className="help-frame" />
-        </Modal>
-        <p>Version 2.1.0 (47)</p>
-      </Layout.Footer>
+          <iframe
+            style={{
+              background: colors.primary,
+              width: '100%',
+              border: 'none',
+              minHeight: '975px',
+            }}
+            title="help"
+            src={HELP_FRAME}
+            className="help-frame"
+          />
+        </Dialog>
+        <Paragraph>Version 2.1.0 (47)</Paragraph>
+      </Pane>
     );
   }
 }
