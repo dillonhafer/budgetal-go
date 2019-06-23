49ca4f78c42edefb00a371c18ef9944e3b3b2e6b
Dillon Hafer <dh@dillonhafer.com>
2019-06-23 11:59:39 -0400

    Add register type

--- shared/api/users.d.ts
+++ shared/api/users.d.ts
@@ -7,3 +7,11 @@ export function PasswordResetRequest({
 }: {
   email: string;
 }): Promise<Response>;
+
+export function RegisterRequest({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}): Promise<Response>;
