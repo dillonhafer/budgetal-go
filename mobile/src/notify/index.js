eeca433362a574d2c54ffc207caff3b177047a90
Dillon Hafer <dh@dillonhafer.com>
2019-06-26 21:02:25 -0400

    Add types to notify

--- mobile/src/notify/index.js
+++ mobile/src/notify/index.js
@@ -1,11 +1,11 @@
-import { Alert } from 'react-native';
+import { Alert } from "react-native";
 
 export const error = (msg, delay) => {
-  global.alertWithType('error', 'Error', msg, { delay });
+  global.alertWithType("error", "Error", msg, { delay });
 };
 
 export const notice = (msg, delay) => {
-  global.alertWithType('success', 'Success', msg, { delay });
+  global.alertWithType("success", "Success", msg, { delay });
 };
 
 export const confirm = ({
@@ -15,22 +15,26 @@ export const confirm = ({
   content,
   onOk,
   onCancel,
+}: {
+  okText: string,
+  title: string,
+  onOk(): void,
 }) => {
   Alert.alert(
-    title || 'Confirm',
-    content || 'Are you sure?',
+    title || "Confirm",
+    content || "Are you sure?",
     [
       {
-        text: cancelText || 'Cancel',
-        style: 'cancel',
+        text: cancelText || "Cancel",
+        style: "cancel",
         onPress: onCancel,
       },
       {
-        text: okText || 'OK',
-        style: 'destructive',
+        text: okText || "OK",
+        style: "destructive",
         onPress: onOk,
       },
     ],
-    { cancelable: true },
+    { cancelable: true }
   );
 };
