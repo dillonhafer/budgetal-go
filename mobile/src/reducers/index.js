d5a76ab2992f385ba3114e3ea65c0a97ec99d5d2
Dillon Hafer <dh@dillonhafer.com>
2019-06-27 22:08:58 -0400

    Remove annual budgets from redux 🙀✨🎉

--- mobile/src/reducers/index.js
+++ mobile/src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from "redux";
 
-import annualBudgetItems from "@src/reducers/AnnualBudgetItems";
 import budget from "@src/reducers/Budget";
 import users from "@src/reducers/Users";
 import netWorth from "@src/reducers/NetWorth";
 
 const appReducers = combineReducers({
-  annualBudgetItems,
   budget,
   users,
   netWorth,
