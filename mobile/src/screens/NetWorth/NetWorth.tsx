5d37c59a0e90ac731d0e0c05e6f5ae0fd72ad4e2
Dillon Hafer <dh@dillonhafer.com>
2019-08-11 11:05:10 -0400

    Performance fix for NetWorth list

--- mobile/src/screens/NetWorth/NetWorth.tsx
+++ mobile/src/screens/NetWorth/NetWorth.tsx
@@ -1,209 +1,232 @@
-import { monthName } from "@shared/helpers";
-import { SplitBackground } from "@src/components/Card";
 import GroupList from "@src/components/GroupList";
 import { PrimaryButton } from "@src/forms";
-import DatePicker from "@src/utils/DatePicker";
-import Device from "@src/utils/Device";
-import Spin from "@src/utils/Spin";
-import gql from "graphql-tag";
-import React, { useEffect, useRef, useState } from "react";
-import { useQuery, useMutation } from "react-apollo";
-import { RefreshControl, StatusBar, View } from "react-native";
-import Carousel from "react-native-snap-carousel";
-import { NavigationScreenConfigProps } from "react-navigation";
-import MonthCard from "./MonthCard";
-import { Asset } from "./types";
-import { GetAssets } from "./__generated__/GetAssets";
-import { GetNetWorth, GetNetWorthVariables } from "./__generated__/GetNetWorth";
-import {
-  AssetLiabilityDelete,
-  AssetLiabilityDeleteVariables,
-} from "./__generated__/AssetLiabilityDelete";
 import { notice } from "@src/notify";
+import gql from "graphql-tag";
+import React, { useEffect, useState, useRef, useCallback } from "react";
+import { useMutation, useQuery } from "react-apollo";
+import { RefreshControl, StatusBar } from "react-native";
+import { NavigationScreenConfigProps } from "react-navigation";
+import { Asset } from "./types";
+import {
+	AssetLiabilityDelete,
+	AssetLiabilityDeleteVariables,
+} from "./__generated__/AssetLiabilityDelete";
+import { GetAssets } from "./__generated__/GetAssets";
+import Device from "@src/utils/Device";
+import styled from "styled-components/native";
+import DatePicker from "@src/utils/DatePicker";
+import {
+	GetNetWorth,
+	GetNetWorthVariables,
+	GetNetWorth_netWorth,
+} from "./__generated__/GetNetWorth";
+import { monthName } from "@shared/helpers";
+import { SplitBackground } from "@src/components/Card";
+import Carousel from "react-native-snap-carousel";
+import MonthCard from "./MonthCard";
 
-const ScreenWidth = Device.width;
 const defaultYear = parseInt(`${new Date().getFullYear()}`);
+const emptyMonths: GetNetWorth_netWorth[] = [];
+for (let i = 1; i < 13; i++) {
+	emptyMonths.push({
+		__typename: "NetWorth",
+		id: `${defaultYear}-${i}`,
+		month: i,
+		year: defaultYear,
+		netWorthItems: [],
+	});
+}
 
+const DatePickerContainer = styled.View({
+	backgroundColor: "white",
+	paddingHorizontal: 5,
+});
+const ScreenWidth = Device.width;
+const ItemWidth = ScreenWidth - 50;
+
+const deleteConfirmation = `⚠️ Are you sure?\nThis will remove all items from past records.\n⛔️ This cannot be undone.`;
 const ASSET_LIABILITY_DELETE = gql`
-  mutation AssetLiabilityDelete($id: ID!) {
-    assetLiabilityDelete(id: $id) {
-      id
-      isAsset
-      name
-    }
-  }
+	mutation AssetLiabilityDelete($id: ID!) {
+		assetLiabilityDelete(id: $id) {
+			id
+			isAsset
+			name
+		}
+	}
 `;
 
 const GET_NET_WORTH = gql`
-  query GetNetWorth($year: Int!) {
-    netWorth(year: $year) {
-      id
-      month
-      netWorthItems {
-        id
-        amount
-        asset {
-          id
-          name
-          isAsset
-        }
-      }
-      year
-    }
-  }
+	query GetNetWorth($year: Int!) {
+		netWorth(year: $year) {
+			id
+			month
+			netWorthItems {
+				id
+				amount
+				asset {
+					id
+					name
+					isAsset
+				}
+			}
+			year
+		}
+	}
 `;
 
 const GET_ASSETS = gql`
-  query GetAssets {
-    assets {
-      id
-      isAsset
-      name
-    }
-  }
+	query GetAssets {
+		assets {
+			id
+			isAsset
+			name
+		}
+	}
 `;
+const initialIndex = new Date().getMonth();
+
+interface HeaderProps {
+	onDateChange: ({ year }: { year: number }) => void;
+	onPress: (item: GetNetWorth_netWorth) => void;
+	items: GetNetWorth_netWorth[];
+}
+
+const HeaderComponent = ({ onDateChange, items, onPress }: HeaderProps) => {
+	const carousel = useRef(null);
+	return (
+		<>
+			<DatePickerContainer>
+				<DatePicker year={defaultYear} onChange={onDateChange} />
+			</DatePickerContainer>
+			<SplitBackground>
+				<Carousel
+					ref={carousel}
+					data={items}
+					shouldOptimizeUpdates
+					initialNumToRender={12}
+					sliderWidth={ScreenWidth}
+					itemWidth={ItemWidth}
+					firstItem={initialIndex}
+					renderItem={({ item }: { item: GetNetWorth_netWorth }) => (
+						<MonthCard item={item} onPress={() => onPress(item)} />
+					)}
+				/>
+			</SplitBackground>
+		</>
+	);
+};
 
 interface Props extends NavigationScreenConfigProps {}
-
 const NetWorthScreen = ({ navigation }: Props) => {
-  const [year, setYear] = useState(defaultYear);
-  const [refreshing, setRefreshing] = useState(false);
-  const { loading: loadingItems, data, refetch } = useQuery<
-    GetNetWorth,
-    GetNetWorthVariables
-  >(GET_NET_WORTH, {
-    variables: { year },
-  });
+	const [refreshing, setRefreshing] = useState(false);
+	const { data: assetData, refetch: refetchAssets } = useQuery<GetAssets>(
+		GET_ASSETS
+	);
+	const assets = assetData && assetData.assets ? assetData.assets : [];
+	const sectionData = [
+		{
+			title: "ASSETS",
+			color: "transparent",
+			data: assets.filter(a => a.isAsset),
+		},
+		{
+			title: "LIABILITIES",
+			color: "transparent",
+			data: assets.filter(a => !a.isAsset),
+		},
+	];
 
-  const {
-    loading: loadingAssets,
-    data: assetData,
-    refetch: refetchAssets,
-  } = useQuery<GetAssets>(GET_ASSETS);
-  const loading = loadingItems || loadingAssets;
+	useEffect(() => {
+		if (refreshing) {
+			refetchAssets();
+			setRefreshing(false);
+		}
+	}, [refreshing]);
 
-  const months = data && data.netWorth ? data.netWorth : [];
-  const assets = assetData && assetData.assets ? assetData.assets : [];
-  const sectionData = [
-    {
-      title: "ASSETS",
-      color: "transparent",
-      data: assets.filter(a => a.isAsset),
-    },
-    {
-      title: "LIABILITIES",
-      color: "transparent",
-      data: assets.filter(a => !a.isAsset),
-    },
-  ];
+	const [deleteAsset] = useMutation<
+		AssetLiabilityDelete,
+		AssetLiabilityDeleteVariables
+	>(ASSET_LIABILITY_DELETE, { refetchQueries: ["GetAssets", "GetNetWorth"] });
 
-  useEffect(() => {
-    if (refreshing) {
-      refetch({ year });
-      refetchAssets();
-      setRefreshing(false);
-    }
-  }, [year, refreshing]);
+	const { data, refetch } = useQuery<GetNetWorth, GetNetWorthVariables>(
+		GET_NET_WORTH,
+		{
+			variables: { year: defaultYear },
+		}
+	);
 
-  const carousel = useRef(null);
+	const emptyData: GetNetWorth_netWorth[] = [];
+	let items = emptyData;
+	if (data && data.netWorth) {
+		items = data.netWorth;
+	}
 
-  const [deleteAsset] = useMutation<
-    AssetLiabilityDelete,
-    AssetLiabilityDeleteVariables
-  >(ASSET_LIABILITY_DELETE, { refetchQueries: ["GetAssets", "GetNetWorth"] });
+	const onPress = (item: GetNetWorth_netWorth) => {
+		const label = monthName(item.month);
+		navigation.navigate("MonthListScreen", {
+			month: {
+				...item,
+				label,
+			},
+			year: item.year,
+		});
+	};
 
-  return (
-    <>
-      <StatusBar barStyle="dark-content" animated={true} />
-      <GroupList
-        refreshControl={
-          <RefreshControl
-            tintColor={"lightskyblue"}
-            refreshing={refreshing}
-            onRefresh={() => {
-              setRefreshing(true);
-            }}
-          />
-        }
-        keyExtractor={(i: Asset) => i.id}
-        sections={sectionData}
-        renderHeader={() => {
-          return (
-            <View>
-              <View style={{ backgroundColor: "#fff", paddingHorizontal: 5 }}>
-                <DatePicker
-                  year={year}
-                  onChange={({ year }) => {
-                    navigation.setParams({ year });
-                    setYear(year);
-                  }}
-                />
-              </View>
-              <SplitBackground>
-                <Carousel
-                  ref={carousel}
-                  data={months}
-                  renderItem={({ item }) => {
-                    const label = monthName(item.month);
-                    return (
-                      <MonthCard
-                        item={item}
-                        onPress={() => {
-                          navigation.navigate("MonthListScreen", {
-                            month: {
-                              ...item,
-                              label,
-                            },
-                            year,
-                            assets: assets.filter(a => a.isAsset),
-                            liabilities: assets.filter(a => !a.isAsset),
-                          });
-                        }}
-                      />
-                    );
-                  }}
-                  sliderWidth={ScreenWidth}
-                  inactiveSlideOpacity={1}
-                  inactiveSlideScale={0.9}
-                  itemWidth={ScreenWidth - 50}
-                  firstItem={new Date().getMonth()}
-                />
-              </SplitBackground>
-            </View>
-          );
-        }}
-        renderSectionFooter={({ section }: { section: { title: string } }) => {
-          const title = section.title === "ASSETS" ? "ASSET" : "LIABILITY";
-          return (
-            <PrimaryButton
-              title={`ADD ${title}`}
-              onPress={() => {
-                navigation.navigate("NewAssetLiabilityScreen", {
-                  section,
-                  title,
-                });
-              }}
-            />
-          );
-        }}
-        onEdit={(item: Asset) => {
-          navigation.navigate("EditAssetLiabilityScreen", {
-            item,
-          });
-        }}
-        deleteConfirmation={`⚠️ Are you sure?\nThis will remove all items from past records.\n⛔️ This cannot be undone.`}
-        onDelete={(asset: Asset) => {
-          const title = asset.isAsset ? "ASSET" : "LIABILITY";
-          deleteAsset({ variables: { id: asset.id } }).then(() => {
-            notice(`DELETED ${title}`);
-          });
-        }}
-        ListFooterComponent={<Spin spinning={loading && !refreshing} />}
-      />
+	const onDateChange = ({ year }: { year: number }) => {
+		refetch({ year });
+	};
 
-      <Spin spinning={loading && !refreshing} />
-    </>
-  );
+	return (
+		<>
+			<StatusBar barStyle="dark-content" animated={true} />
+			<GroupList
+				refreshControl={
+					<RefreshControl
+						tintColor={"lightskyblue"}
+						refreshing={refreshing}
+						onRefresh={() => {
+							setRefreshing(true);
+						}}
+					/>
+				}
+				keyExtractor={(i: Asset) => i.id}
+				sections={sectionData}
+				renderHeader={
+					<HeaderComponent
+						items={items}
+						onPress={onPress}
+						onDateChange={onDateChange}
+					/>
+				}
+				renderSectionFooter={({ section }: { section: { title: string } }) => {
+					const title = section.title === "ASSETS" ? "ASSET" : "LIABILITY";
+					return (
+						<PrimaryButton
+							title={`ADD ${title}`}
+							onPress={() => {
+								navigation.navigate("NewAssetLiabilityScreen", {
+									section,
+									title,
+								});
+							}}
+						/>
+					);
+				}}
+				onEdit={(item: Asset) => {
+					navigation.navigate("EditAssetLiabilityScreen", {
+						item,
+					});
+				}}
+				deleteConfirmation={deleteConfirmation}
+				onDelete={(asset: Asset) => {
+					const title = asset.isAsset ? "ASSET" : "LIABILITY";
+					deleteAsset({ variables: { id: asset.id } }).then(() => {
+						notice(`DELETED ${title}`);
+					});
+				}}
+			/>
+		</>
+	);
 };
 
 export default NetWorthScreen;
