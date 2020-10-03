4bab76c22f04f006cb8d22533c7b0a764519b118
Dillon Hafer <dh@dillonhafer.com>
2020-10-03 18:00:34 -0400

    Add missing package expo-screen-orientation

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,10 +3,11 @@ import RootNavigator from "@src/navigators/root";
 import { createApolloClient } from "@src/utils/apollo";
 import Device from "@src/utils/Device";
 import { preloadAssetsAsync } from "@src/utils/preload-assets";
-import { AppLoading, Linking, ScreenOrientation } from "expo";
+import { AppLoading, Linking } from "expo";
 import React, { useState } from "react";
 import { ApolloProvider } from "react-apollo";
 import { Platform, StatusBar } from "react-native";
+import * as ScreenOrientation from "expo-screen-orientation";
 
 StatusBar.setBarStyle("light-content", true);
 const client = createApolloClient();
@@ -14,30 +15,30 @@ const prefix = Linking.makeUrl("/");
 
 // Allow iPads to use landscape
 if (Platform.OS === "ios" && Device.isTablet()) {
-  ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.ALL);
+	ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.ALL);
 } else {
-  ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
+	ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
 }
 
 const App = () => {
-  const [loaded, setLoaded] = useState(false);
+	const [loaded, setLoaded] = useState(false);
 
-  if (!loaded) {
-    return (
-      <AppLoading
-        startAsync={preloadAssetsAsync}
-        onFinish={() => setLoaded(true)}
-        onError={console.warn}
-      />
-    );
-  }
+	if (!loaded) {
+		return (
+			<AppLoading
+				startAsync={preloadAssetsAsync}
+				onFinish={() => setLoaded(true)}
+				onError={console.warn}
+			/>
+		);
+	}
 
-  return (
-    <ApolloProvider client={client}>
-      <RootNavigator uriPrefix={prefix} />
-      <DropdownAlert />
-    </ApolloProvider>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<RootNavigator uriPrefix={prefix} />
+			<DropdownAlert />
+		</ApolloProvider>
+	);
 };
 
 export default App;
